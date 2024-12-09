cc04eba6c4e98a128cc65bd8b8bc7a1d7b1b133e
Mark <mark.mazur007@gmail.com>
2024-12-09 18:01:19 -0500

    add .isna, .notna and etc in missing_data.py

--- scripts/pandas_learn/missing_data.py
+++ scripts/pandas_learn/missing_data.py
@@ -3,11 +3,14 @@ import pandas as pd
 # creating DataFrame with missing values
 data = {
     'name': ['Alice', 'Bob', None],
-    'age': [None, 22, 30],
+    'age': [30, 22, None],
     'city': [None, 'Los Angeles', 'New York']
 }
 
 df = pd.DataFrame(data=data)
+
+# print info about DataFrame to see the missing values
+print(f'DataFrame info:\n{df.info()}\n')
 
 # check for missing values
 print(f'Check for missing values:\n{df.isnull()}\n')
@@ -23,3 +26,10 @@ print(f'DataFrame after filling missing values:\n{df_filled}\n')
 # interpolate missing values (for numerical columns)
 df_interpolated = df['age'].interpolate()
 print(f'DataFrame after interpolating missing values in "age":\n{df_interpolated}\n')
+
+# check for NaN and non-NaN values
+print(f'Check if values is NaN:\n{df['name'].isna()}\n')
+print(f'Check if values is NOT NaN:\n{df['name'].notna()}\n')
+
+# count missing values per column
+print(f'Counting NaN values per column:\n{df.isna().sum()}\n')
