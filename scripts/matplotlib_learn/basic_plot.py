ad943824aca27dc1a6a3997688558b1c9d70a611
Mark <mark.mazur007@gmail.com>
2024-12-14 23:43:55 -0500

    remove unnecessary code in basic_plot.py

--- scripts/matplotlib_learn/basic_plot.py
+++ scripts/matplotlib_learn/basic_plot.py
@@ -6,10 +6,9 @@ x = np.linspace(0, 10, 100) # make random data
 y = np.sin(x)
 
 # basic of plotting
-plt.plot(x, y, label='y=sin(x)', color='red') # plot the graph
-plt.title('Basic Plot', fontsize=16) # the name of plot at the top
-plt.xlabel('x-axis', fontsize=12) # the name of x-axis
-plt.ylabel('y-axis', fontsize=12) # the name of y-axis
+plt.plot(x, y, label='y=sin(x)') # plot the graph
+plt.title('Basic Plot') # the name of plot at the top
+plt.xlabel('x-axis') # the name of x-axis
+plt.ylabel('y-axis') # the name of y-axis
 plt.legend() # draw the labels at the right top
-plt.grid(True) # draw a grid in the plot
 plt.show() # show the plot
